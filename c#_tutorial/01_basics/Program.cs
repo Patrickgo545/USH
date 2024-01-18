c3c8f5b14e72efce0c10675664d305c61b87b077
Patrick Go <patrickgo545@gmail.com>
2024-01-18 16:42:20 -0500

    C# Course - Start lesson 3

---
+++ c#_tutorial/01_basics/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace my_awesome_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        // {
+        //     // CHANGE APPEARANCE
+        //     Console.Title = "Skynet";
+        //     Console.ForegroundColor = ConsoleColor.Green;
+        //     Console.WindowHeight = 40;
+
+        //     // DIALOGUE
+        //     Console.WriteLine("Hello, what's your name?");
+        //     string userName = Console.ReadLine();
+
+        //     Console.WriteLine($"Hello {userName} My name is RX-9000, im an AI here to destroy your race. \nWhat is your favorite color?");
+        //     string userColor = Console.ReadLine();
+ 
+        //     Console.WriteLine("Very nice. Mine is destruction.");
+
+        //     // END PROGRAM
+        //     Console.ReadKey();
+        // }
+
+        {
+            // PRACTICE
+            Console.Title = "Last Light Inn";
+            Console.WindowHeight = 40;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // DIALOGUE
+            Console.WriteLine("Ayy Traveller, what be thy name?");
+            string travellerName = Console.ReadLine();
+
+            Console.WriteLine($"Ay {travellerName} How many beasts did ye slay today?");
+            string travellerKillCount = Console.ReadLine(); 
+
+            Console.WriteLine($"Ayy.. well I killed more beasts than {travellerKillCount}. \nDo better");
+
+            // KEEP WINDOW OPEN
+            Console.ReadKey();
+        }
+    }
+}
--- c#_tutorial/02_variables/Program.cs
+++ c#_tutorial/02_variables/Program.cs
@@ -5,8 +5,44 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
         static void Main(string[] args)
+        // {
+        //     int num01;
+        //     int num02;
+
+        //     Console.Write("Input a number: ");
+        //     num01 = Convert.ToInt32(Console.ReadLine());
+
+        //     Console.Write("Input a second number: ");
+        //     num02 = Convert.ToInt32(Console.ReadLine());
+
+        //     int result = num01 * num02;
+        //     Console.WriteLine($"{num01} * {num02} = {result}");
+            
+        //     // WAIT TO CLOSE
+        //     Console.ReadKey();
+        // }
+
         {
-            Console.WriteLine("Hello World!");
+            // CHALLENGE FIND THE AVERAGE OF 3 NUMBERS
+            Console.WriteLine("I'm going to help you find the average of 3 numbers");
+            
+            double num01;
+            Console.WriteLine("number 1: ");
+            num01 = Convert.ToDouble(Console.ReadLine());
+
+            double num02;
+            Console.WriteLine("number 2: ");
+            num02 = Convert.ToDouble(Console.ReadLine());
+
+            double num03;
+            Console.WriteLine("number 3: ");
+            num03 = Convert.ToDouble(Console.ReadLine());
+
+            double result = (num01 + num02 + num03) / 3;
+            Console.WriteLine($"The average is {result}");
+
+            // WAIT TO CLOSE
+            Console.ReadKey();
         }
     }
 }
