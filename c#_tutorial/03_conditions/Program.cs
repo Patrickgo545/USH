948a1c9629ffc977946814e00a00bc640e273e39
Patrick Go <patrickgo545@gmail.com>
2024-01-19 13:27:15 -0500

    C# - Lesson 3 Conditions

--- c#_tutorial/03_conditions/Program.cs
+++ c#_tutorial/03_conditions/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace conditions // Note: actual namespace depends on the project name.
+namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TICKETS
+            // Console.WriteLine("Welcome! Tickets are $5. Please insert cash.");
+
+            // int cash = Convert.ToInt32(Console.ReadLine());
+
+            // if (cash < 5) 
+            // {  
+            //     Console.WriteLine("That's not enough money.");
+            // }
+            // else if (cash == 5)
+            // {
+            //     Console.WriteLine("Here is your ticket");
+            // }
+
+            // else 
+            // {
+            //     int change = cash - 5;
+            //     Console.WriteLine($"Here is your Ticket & your change ${change}");
+            // }
+
+            
+            // ROLLER COASTER
+            int age;
+            int height;
+
+            int min_age = 18;
+            int min_height = 160;
+
+            Console.Write("What is your age? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is your height? ");
+            height = Convert.ToInt16(Console.ReadLine());
+
+            if (age >= min_age && height >= min_height) 
+            {
+                Console.Write("You may enter.");
+            }
+
+            else if (age < min_age && height >= min_height)
+            {
+                Console.WriteLine("Not old enough.");
+            }
+
+            else if (height < min_height && age >= min_age)
+            {
+                Console.WriteLine("Not tall enough.");
+            }
+
+            else
+            {
+                Console.Write("Get out of here.");
+            }
         }
     }
 }
--- c#_tutorial/03_conditions/obj/Debug/net7.0/03_conditions.AssemblyInfo.cs
+++ c#_tutorial/03_conditions/obj/Debug/net7.0/03_conditions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("03_conditions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e32d01b17b1c7c00c575eb1c4a5d339478278d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("03_conditions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("03_conditions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
