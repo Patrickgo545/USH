26a6d29cd9fad5b0a7ac2e0c0536ad7f820eca7c
Patrick Go <patrickgo545@gmail.com>
2024-01-22 15:51:25 -0500

    C# Mentoring Session - Visual Studio (Building Projects)

---
+++ c#_tutorial/Mentoring_Session/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentoring_Session
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            MailMessage mm = new MailMessage(
+                "tech@ushunger.org", "patrick@ushunger.org", "TESTING", "Yayah!");
+
+            // Set up the SMTP client
+            using (var client = new SmtpClient("smtp.gmail.com", 587))
+            {
+                client.EnableSsl = true;
+                // Authenticate with your email credentials
+                // Replace these values with your actual email credentials
+                string username = "tech@ushunger.org";
+                string password = "mxhlizlvdrmgeqyn";
+
+                // Create an instance of NetworkCredential
+                ICredentialsByHost credentials = new NetworkCredential(username, password);
+
+                client.Credentials = credentials;
+
+                // Send the email
+                client.Send(mm);
+
+                // Disconnect from the SMTP server
+
+            }
+
+            Console.WriteLine("Email sent successfully.");
+        }
+    }
+}
