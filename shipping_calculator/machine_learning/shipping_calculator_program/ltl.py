b0acc23552538f39c4b84609535540b1305fce58
Patrick Go <patrickgo545@gmail.com>
2024-03-27 23:17:25 -0400

    Added LTL.py module - created class for module

--- shipping_calculator/machine_learning/shipping_calculator_program/ltl.py
+++ shipping_calculator/machine_learning/shipping_calculator_program/ltl.py
@@ -1,1 +1,32 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 from data import df
+
+
+
+
+class LtlModule:
+    def __init__(self, df):
+        self.df = df 
+
+    def TrainModule(self):
+        X = df[['pallets', 'weight', 'distance', 'avg_gas_price']]
+
+        # DEPENDENT VARIABLE
+        y = df['quote']
+
+        # SPLIT DATASET
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+
+        # TRAINING MODEL
+        regressor = LinearRegression()
+        regressor.fit(X_train,y_train)
+
+        # VALIDATING MODEL
+        y_pred = regressor.predict(X_test)
+
+        return regressor
+
+
+#coefficients =     
