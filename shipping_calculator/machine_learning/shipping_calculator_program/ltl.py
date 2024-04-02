ca1d198877427b3f1e3ec7f246978ab278b76eac
Patrick Go <patrickgo545@gmail.com>
2024-04-02 17:16:21 -0400

    ltl Module

--- shipping_calculator/machine_learning/shipping_calculator_program/ltl.py
+++ shipping_calculator/machine_learning/shipping_calculator_program/ltl.py
@@ -1,23 +1,31 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+import pandas as pd
 from data import df
 
 
 
+class TrainModule:
 
-class LtlModule:
-    def __init__(self, df):
+    def __init__(self, df, coef_labels):
         self.df = df 
+        self.coef_labels  = coef_labels
+        self.results = None
+        self.variables = None
+        self.coefficients = None
+        self.regressor = self.TrainModule()
+
+
 
     def TrainModule(self):
-        X = df[['pallets', 'weight', 'distance', 'avg_gas_price']]
+        self.x_variables = self.df[[self.coef_labels]]
 
         # DEPENDENT VARIABLE
-        y = df['quote']
+        y = self.df['quote']
 
         # SPLIT DATASET
-        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(self.x_variables,y,test_size=0.2, random_state=0)
 
         # TRAINING MODEL
         regressor = LinearRegression()
@@ -25,8 +33,17 @@ class LtlModule:
 
         # VALIDATING MODEL
         y_pred = regressor.predict(X_test)
+        self.results = pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})
 
-        return regressor
+        # EXTRACT COEFFICIENTS
+        self.coefficients = self.regressor.coef_
+        self.variables = self.x_variables.columns         
 
+        return self.regressor
+    
 
-#coefficients =     
+    # def ExtractCoef(self):
+    #     coefficients = self.regressor.coef_
+    #     variable_names = self.x_variables.columns 
+
+    # TODO: Ship results into CSV for inspections
--- shipping_calculator/machine_learning/shipping_calculator_program/main.py
+++ shipping_calculator/machine_learning/shipping_calculator_program/main.py
@@ -1,2 +1,11 @@
 import pandas as pd
+from ltl import TrainModule
+from data import df
+
+coef_labels_ltl = ['pallets', 'weight', 'distance', 'avg_gas_price']
+
+
+
+ltl = TrainModule(df=df, coef_labels=coef_labels_ltl)
+
 
