3b1ae185671dd153ef82cb1cc9ced274ec2ea29b
Patrick Go <patrickgo545@gmail.com>
2024-10-25 14:11:37 -0400

    Added Dictionary with all shipment types & correlating x-variables
    for loop & result dictionary for all tracked coefficients for each shipment type

--- shipping_calculator/main.py
+++ shipping_calculator/main.py
@@ -3,9 +3,13 @@ from data import df
 from regressors import CoefficientModule
 import json
 
+result = {}
 
 shipment_variables = {
-    "ltl": ["pallets", "weight", "distance", "avg_gas_price"]
+    "LTL": ["pallets", "weight", "distance", "avg_gas_price"],
+    "FTL": ["weight", "distance", "avg_gas_price"],
+    "Box Truck": ["pallets", "weight", "distance", "avg_gas_price"],
+    "Sprinter Van": ["distance", "avg_gas_price"]
 }
 
 
@@ -19,6 +23,14 @@ def ExtractCoefficients(shipment_type, variable_list):
     
     return coefficient_dict
 
-shipment_var = shipment_variables["ltl"]
-ltl = ExtractCoefficients("LTL", shipment_var)
-print(ltl)
+
+# shipment_var = shipment_variables["LTL"]
+# ltl = ExtractCoefficients("LTL", shipment_var)
+# print(ltl)
+
+for shipment_type, variables in shipment_variables.items():
+    coefficients = ExtractCoefficients(shipment_type, variables)
+    
+    result[shipment_type] = coefficients
+    
+print(result)
